52070593c68126a858dfa748085e09d5265752a2
Diana Grams <deegrams221@gmail.com>
2020-08-19 22:08:51 -0600

    buttons functioning

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { FontAwesome } from '@expo/vector-icons';
+import { Button } from 'native-base';
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
@@ -43,7 +44,7 @@ export default class App extends React.Component {
     } else if (itemArray[itemNum] === 'unlucky') {
       return 'frown-o';
     }
-    return 'minus-circle';
+    return 'circle';
   };
 
   // find the correct color
@@ -75,7 +76,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View>
-          <Text>new app yaya</Text>
+          <Text style={styles.containertext}>Scratch and Win Game!</Text>
         </View>
         <View style={styles.grid}>
           <View style={styles.itemrow}>
@@ -144,123 +145,61 @@ export default class App extends React.Component {
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(0);
+                this.scratchItem(5);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(0)}
+                name={this.scratchItemIcon(5)}
                 size={50}
-                color={this.scratchItemColor(0)}
+                color={this.scratchItemColor(5)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(1);
+                this.scratchItem(6);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(1)}
+                name={this.scratchItemIcon(6)}
                 size={50}
-                color={this.scratchItemColor(1)}
+                color={this.scratchItemColor(6)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(2);
+                this.scratchItem(7);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(2)}
+                name={this.scratchItemIcon(7)}
                 size={50}
-                color={this.scratchItemColor(2)}
+                color={this.scratchItemColor(7)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(3);
+                this.scratchItem(8);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(3)}
+                name={this.scratchItemIcon(8)}
                 size={50}
-                color={this.scratchItemColor(3)}
+                color={this.scratchItemColor(8)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(4);
+                this.scratchItem(9);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(4)}
+                name={this.scratchItemIcon(9)}
                 size={50}
-                color={this.scratchItemColor(4)}
-              />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.itemrow}>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => {
-                this.scratchItem(0);
-              }}
-            >
-              <FontAwesome
-                name={this.scratchItemIcon(0)}
-                size={50}
-                color={this.scratchItemColor(0)}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => {
-                this.scratchItem(1);
-              }}
-            >
-              <FontAwesome
-                name={this.scratchItemIcon(1)}
-                size={50}
-                color={this.scratchItemColor(1)}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => {
-                this.scratchItem(2);
-              }}
-            >
-              <FontAwesome
-                name={this.scratchItemIcon(2)}
-                size={50}
-                color={this.scratchItemColor(2)}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => {
-                this.scratchItem(3);
-              }}
-            >
-              <FontAwesome
-                name={this.scratchItemIcon(3)}
-                size={50}
-                color={this.scratchItemColor(3)}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.item}
-              onPress={() => {
-                this.scratchItem(4);
-              }}
-            >
-              <FontAwesome
-                name={this.scratchItemIcon(4)}
-                size={50}
-                color={this.scratchItemColor(4)}
+                color={this.scratchItemColor(9)}
               />
             </TouchableOpacity>
           </View>
@@ -268,61 +207,61 @@ export default class App extends React.Component {
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(0);
+                this.scratchItem(10);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(0)}
+                name={this.scratchItemIcon(10)}
                 size={50}
-                color={this.scratchItemColor(0)}
+                color={this.scratchItemColor(10)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(1);
+                this.scratchItem(11);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(1)}
+                name={this.scratchItemIcon(11)}
                 size={50}
-                color={this.scratchItemColor(1)}
+                color={this.scratchItemColor(11)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(2);
+                this.scratchItem(12);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(2)}
+                name={this.scratchItemIcon(12)}
                 size={50}
-                color={this.scratchItemColor(2)}
+                color={this.scratchItemColor(12)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(3);
+                this.scratchItem(13);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(3)}
+                name={this.scratchItemIcon(13)}
                 size={50}
-                color={this.scratchItemColor(3)}
+                color={this.scratchItemColor(13)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(4);
+                this.scratchItem(14);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(4)}
+                name={this.scratchItemIcon(14)}
                 size={50}
-                color={this.scratchItemColor(4)}
+                color={this.scratchItemColor(14)}
               />
             </TouchableOpacity>
           </View>
@@ -330,65 +269,147 @@ export default class App extends React.Component {
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(0);
+                this.scratchItem(15);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(0)}
+                name={this.scratchItemIcon(15)}
                 size={50}
-                color={this.scratchItemColor(0)}
+                color={this.scratchItemColor(15)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(1);
+                this.scratchItem(16);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(1)}
+                name={this.scratchItemIcon(16)}
                 size={50}
-                color={this.scratchItemColor(1)}
+                color={this.scratchItemColor(16)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(2);
+                this.scratchItem(17);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(2)}
+                name={this.scratchItemIcon(17)}
                 size={50}
-                color={this.scratchItemColor(2)}
+                color={this.scratchItemColor(17)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(3);
+                this.scratchItem(18);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(3)}
+                name={this.scratchItemIcon(18)}
                 size={50}
-                color={this.scratchItemColor(3)}
+                color={this.scratchItemColor(18)}
               />
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.item}
               onPress={() => {
-                this.scratchItem(4);
+                this.scratchItem(19);
               }}
             >
               <FontAwesome
-                name={this.scratchItemIcon(4)}
+                name={this.scratchItemIcon(19)}
                 size={50}
-                color={this.scratchItemColor(4)}
+                color={this.scratchItemColor(19)}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.itemrow}>
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                this.scratchItem(20);
+              }}
+            >
+              <FontAwesome
+                name={this.scratchItemIcon(20)}
+                size={50}
+                color={this.scratchItemColor(20)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                this.scratchItem(21);
+              }}
+            >
+              <FontAwesome
+                name={this.scratchItemIcon(21)}
+                size={50}
+                color={this.scratchItemColor(21)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                this.scratchItem(22);
+              }}
+            >
+              <FontAwesome
+                name={this.scratchItemIcon(22)}
+                size={50}
+                color={this.scratchItemColor(22)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                this.scratchItem(23);
+              }}
+            >
+              <FontAwesome
+                name={this.scratchItemIcon(23)}
+                size={50}
+                color={this.scratchItemColor(23)}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                this.scratchItem(24);
+              }}
+            >
+              <FontAwesome
+                name={this.scratchItemIcon(24)}
+                size={50}
+                color={this.scratchItemColor(24)}
               />
             </TouchableOpacity>
           </View>
         </View>
+        <Button
+          onPress={() => {
+            this.showAllItemBtn();
+          }}
+          full
+          success
+          style={styles.button}
+        >
+          <Text style={styles.buttontext}>Show All Coupons</Text>
+        </Button>
+        <Button
+          onPress={() => {
+            this.resetGameBtn();
+          }}
+          full
+          primary
+          style={styles.button}
+        >
+          <Text style={styles.buttontext}>Reset Game</Text>
+        </Button>
       </View>
     );
   }
@@ -401,6 +422,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containertext: {
+    fontSize: 30,
+  },
   grid: {},
   itemrow: {
     flexDirection: 'row',
@@ -411,5 +435,12 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#000',
     minWidth: 70,
+  },
+  button: {
+    marginVertical: 15,
+  },
+  buttontext: {
+    color: '#FFF',
+    fontSize: 20,
   },
 });
